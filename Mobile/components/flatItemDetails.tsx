3fadcb7acca7b9d8bc9d3419878c5a8cd20d0232
Piotr Kawa <piotr0589.pk@gmail.com>
2023-01-29 20:55:07 +0100

    Dodanie stylingu, dokończenie widoku mieszkań

--- Mobile/components/flatItemDetails.tsx
+++ Mobile/components/flatItemDetails.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import { Flat } from './flatItem';
 
 type FlatItemDetailsProps = {
@@ -8,25 +9,88 @@ type FlatItemDetailsProps = {
 };
 
 export const FlatItemDetailsScreen: React.FunctionComponent<FlatItemDetailsProps> = ({route} : FlatItemDetailsProps) => {
-    const flat = route.params.flatItem;
+    const flat : Flat = route.params.flatItem;
     const loginToken = route.params.loginToken;
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState<any[]>([flat.thumbnail]);
+
+    function fetchImages() {
+        const requestOptions = {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json',
+          'Authorization': "Bearer " + loginToken
+         },
+    
+      };
+        setLoading(true);
+        fetch('https://pw-flatly.azurewebsites.net/flats/' + flat.id + '/images', requestOptions)
+          .then((response) => response.json())
+          .then((json) => {
+            setData([...data, ...json])}) 
+          .catch((error) => {})
+          .finally(() => {
+        setLoading(false)})
+      }
+
+      React.useEffect(() => {
+        fetchImages();
+      }, []);
 
     return (
-        <View style={styles.container}>
-            <Text>Flat Details</Text>
-            <Text>Address: {flat.address}</Text>
-            <Text>Size: {flat.footage} </Text>
-            <Text>Rooms: {flat.rooms}</Text>
-            <Text>Price: ${flat.price}</Text>
-        </View>
+<View style={styles.container}>
+    <ScrollView>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Country: </Text>{flat.country}</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Town: </Text>{flat.town}</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Street: </Text>{flat.address}</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Rooms: </Text>{flat.rooms}</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Capacity: </Text>{flat.capacity}</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Footage: </Text>{flat.footage}</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Price: </Text>{flat.price}$</Text>
+            <Text style={styles.subheaderText}><Text style={styles.sectionHeaderText}>Contact info: </Text>{flat.contactInfo}</Text>
+            <Text style={styles.headerText}>Photos:</Text>
+            <ScrollView style={styles.scrollView} horizontal={true} showsHorizontalScrollIndicator={false}>
+                {data.map((image, index) => (
+                    <Image key={index} source={{uri: `data:image/png;base64,${image}`}} style={styles.image} />
+                ))}
+            </ScrollView>
+            <Text style={styles.sectionHeaderText}>Description:</Text>
+            <Text style={styles.subheaderText}>{flat.description}</Text>
+    </ScrollView>
+</View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         padding: 20,
+        alignItems: "flex-start",
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginVertical: 20,
+        alignSelf: "center"
+    },
+    subheaderText: {
+        fontSize: 18,
+        marginVertical: 10,
+        
+    },
+    sectionHeaderText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginVertical: 20,
+        textAlign: 'left'
+    },
+    scrollView: {
+        marginTop: 10,
+        marginBottom: 20,
+        alignSelf: 'center'
+    },
+    image: {
+        width: 150,
+        height: 150,
+        marginRight: 10,
     },
 });
